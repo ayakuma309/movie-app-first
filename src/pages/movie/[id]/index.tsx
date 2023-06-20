e1a5b8663bef1c2896ba2472ce5eb49618120744
ayakuma309 <ayako.30901@gmail.com>
2023-06-20 14:32:23 +0900

    style: おすすめ映画表示まで

--- src/pages/movie/[id]/index.tsx
+++ src/pages/movie/[id]/index.tsx
@@ -2,22 +2,28 @@ import { NextPage, GetServerSideProps } from 'next';
 import React from 'react';
 import { RecommendProps } from '@/types/Types';
 import useMovies from '@/hooks/useMovies';
+import Image from 'next/image';
+import Layout from '@/components/common/Layout';
 
 
 const RecommendMovie: NextPage<RecommendProps> = ({ id }: RecommendProps) => {
   const { movieDetail, movieRecommend } = useMovies(id);
   const { results } = movieRecommend;
-  console.log(movieRecommend.results);
   return (
-    <div>
-      {results && results.map((movie) => (
-        <div key={movie.id}>
-          <div>{movie.title}</div>
-          <div>{movie.poster_path}</div>
-          <div>{movie.overview}</div>
-        </div>
-      ))}
-    </div>
+    <Layout title="おすすめの映画">
+      <div className='flex flex-wrap justify-center mt-24'>
+        {results && results.map((movie) => (
+          <div key={movie.id}>
+            <Image
+              src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`}
+              alt={movie.title}
+              width={100}
+              height={100}
+            />
+          </div>
+        ))}
+      </div>
+    </Layout>
   );
 };
 
