59d27480cf128019cf2cf1568aec04d86e29573a
ayakuma309 <ayako.30901@gmail.com>
2023-06-20 11:51:29 +0900

    feat: 検索表示

---
+++ src/components/search/MovieItem.tsx
@@ -0,0 +1,34 @@
+import React from 'react'
+import { MovieItemProps } from '../../types/Types';
+import { NextPage } from 'next';
+
+import { useRouter } from 'next/router';
+import Image from 'next/image';
+
+const RecommendItem: NextPage<MovieItemProps> = ({ movies }) => {
+  const router = useRouter();
+  return (
+    <div className="flex flex-wrap mx-auto justify-center">
+      {movies && movies
+        .filter((movie) => movie.poster_path)
+        .map((movie) => (
+          <div className="card_movie mx-auto" key={movie.id}>
+            <Image
+              src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`}
+              alt={movie.title}
+              width={200}
+              height={300}
+            />
+            <p>{movie.title}</p>
+            <button
+              className="flex items-center justify-center w-full px-10 py-4 text-base font-medium text-center text-neutral-900 transition duration-500 ease-in-out transform bg-gradient-to-r from-indigo-600 to-indigo-300 rounded-xl hover:from-indigo-300 hover:to-indigo-600 hover:text-white"
+              >
+              おすすめを探す！
+            </button>
+          </div>
+        ))}
+    </div>
+  )
+}
+
+export default RecommendItem
--- src/types/Types.d.ts
+++ src/types/Types.d.ts
@@ -1,4 +1,24 @@
+//Layout.tsx
 export interface CommonTypeProps {
   children: React.ReactNode;
   title: string;
+}
+
+//search.tsx
+export interface MovieInfo {
+  id: number;
+  title: string;
+  poster_path: string;
+  overview: string;
+}
+
+
+//MovieItem.tsx
+export interface MovieItemProps {
+  movies: MovieInfo[];
+}
+
+
+export interface RecommendProps {
+  id: number;
 }
