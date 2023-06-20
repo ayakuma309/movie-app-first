0db0002229ea04c89f245dab278aced332fbf8b2
ayakuma309 <ayako.30901@gmail.com>
2023-06-20 14:23:32 +0900

    feat: 詳細とおすすめのデータ取得

--- src/pages/movie/[id]/index.tsx
+++ src/pages/movie/[id]/index.tsx
@@ -1,12 +1,53 @@
 import { NextPage, GetServerSideProps } from 'next';
-import React from 'react';
+import React, { useEffect } from 'react';
 import { RecommendProps } from '@/types/Types';
 
+
 const RecommendMovie: NextPage<RecommendProps> = ({ id }: RecommendProps) => {
+
+  const [movieDetail, setMovieDetail] = React.useState({});
+  const [movieRecommend, setMovieRecommend] = React.useState({});
+  // const [movieImages, setMovieImages] = React.useState([]);
+  const APIKEY = process.env.NEXT_PUBLIC_MOVIE_API_KEY;
+
+  //firebaseのデータ取得後にmovie.idを使いdetailのAPIで取得
+  useEffect(() => {
+    const fetchMovieDetail = async () => {
+      try {
+        if(id){
+          const url = `https://api.themoviedb.org/3/movie/${id}?api_key=${APIKEY}&language=ja`;
+          const res = await fetch(url);
+          const data = await res.json();
+          setMovieDetail(data);
+        }
+      } catch (error) {
+        console.error(error);
+      }
+    };
+    fetchMovieDetail();
+  }, [id]);
+
+  useEffect(() => {
+    const fetchMovieRecommend = async () => {
+      try {
+        if(id){
+          const url = `https://api.themoviedb.org/3/movie/${id}/recommendations?api_key=${APIKEY}&language=ja&page=1`;
+          const res = await fetch(url);
+          const data = await res.json();
+          setMovieRecommend(data);
+        }
+      } catch (error) {
+        console.error(error);
+      }
+    };
+    fetchMovieRecommend();
+  }, [id]);
+  console.log(movieRecommend);
   return (
     <div>{id}</div>
   );
 };
+
 
 export default RecommendMovie;
 
