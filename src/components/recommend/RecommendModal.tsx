85d1e43522294bd4013861766a9fe1d48f32aaf2
ayakuma309 <ayako.30901@gmail.com>
2023-06-21 13:54:22 +0900

    feat: おすすめ表示の詳細をモーダルで表示

---
+++ src/components/recommend/RecommendModal.tsx
@@ -0,0 +1,58 @@
+import React, { memo, useState } from 'react'
+import Image from 'next/image'
+import { MovieInfo } from '@/types/Types'
+//styles
+import { Modal } from "@mui/material";
+import Link from 'next/link';
+
+const RecommendModal: React.FC<MovieInfo> = ({id, poster_path, title, overview }) => {
+  //modal用
+  const [open, setOpen] = useState(false);
+  return (
+    <div>
+      <div
+        className="glass_card glass_recommend_card"
+        onClick={() => setOpen(true)}
+      >
+        <Image
+          src={`https://image.tmdb.org/t/p/w500/${poster_path}`}
+          alt={title}
+          width={100}
+          height={100}
+          className='mx-auto'
+        />
+        <div className="whitespace-pre-line font-semibold text-gray-700 sm:text-base text-sm">
+          {title}
+        </div>
+      </div>
+      <Modal open={open} onClose={() => setOpen(false)}>
+        <div className="modal-card">
+          <Image
+            src={`https://image.tmdb.org/t/p/w500/${poster_path}`}
+            alt={title}
+            width={200}
+            height={100}
+            className='mx-auto'
+          />
+          <div className="flex justify-center">
+            <div className="text-gray-500 my-2">
+              <span className="text-2xl font-bold">{title}</span>
+              <Link href={`/movie/${id}`}>
+                <button
+                className="btn-color font-bold py-2 px-2 rounded mx-2 border border-gray-900"
+                >
+                  詳細へ
+                </button>
+              </Link>
+            </div>
+          </div>
+          <div className="flex justify-center whitespace-pre-line font-semibold text-gray-700 text-xl mb-3">
+            {overview}
+          </div>
+        </div>
+      </Modal>
+    </div>
+  )
+}
+
+export default memo(RecommendModal);
--- src/pages/movie/[id]/index.tsx
+++ src/pages/movie/[id]/index.tsx
@@ -4,11 +4,12 @@ import { RecommendProps } from '@/types/Types';
 import useMovies from '@/hooks/useMovies';
 import Image from 'next/image';
 import Layout from '@/components/common/Layout';
+import RecommendModal from '@/components/recommend/RecommendModal';
 
 
 const RecommendMovie: NextPage<RecommendProps> = ({ id }: RecommendProps) => {
   const { movieDetail, movieRecommend, trailerUrl } = useMovies(id);
-
+  console.log(movieDetail);
   return (
     <Layout title="Recommended movies">
       <div className='w-9/12  mx-auto font-mono'>
@@ -16,56 +17,53 @@ const RecommendMovie: NextPage<RecommendProps> = ({ id }: RecommendProps) => {
           <div className='text-4xl font-bold text-center'>Recommended movies</div>
           <div className='flex flex-wrap justify-center mt-10'>
             {movieRecommend && movieRecommend.results.map((movie) => (
-              <div key={movie.id}>
-                <div className='glass_card glass_recommend_card'>
-                  <Image
-                    src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`}
-                    alt={movie.title}
-                    width={100}
-                    height={100}
-                    className='mx-auto'
-                  />
-                  <div className='text-gray-600 font-bold mt-2'>{movie.title}</div>
-                </div>
-              </div>
+              <RecommendModal
+                key={movie.id}
+                id={movie.id}
+                title={movie.title}
+                poster_path={movie.poster_path}
+                overview={movie.overview}
+              />
             ))}
           </div>
-          {movieDetail &&
-            <div className='glass_card my-24'>
-              <div className='my-10'>
-                <div className='text-4xl font-bold text-center'>{movieDetail.original_title}<br/>
-                  <span className='text-gray-600 text-xl'>{movieDetail.title}</span>
+          <div className='glass_card my-24'>
+            {movieDetail &&
+              <div>
+                <div className='my-10'>
+                  <div className='text-4xl font-bold text-center'>{movieDetail.original_title}<br/>
+                    <span className='text-gray-600 text-xl'>{movieDetail.title}</span>
+                  </div>
                 </div>
+                {movieDetail.poster_path &&
+                  <Image
+                    src={`https://image.tmdb.org/t/p/w500/${movieDetail.poster_path}`}
+                    alt={movieDetail.title}
+                    width={200}
+                    height={200}
+                    className='mx-auto'
+                  />
+                }
+                <div className='flex justify-center mt-5'>
+                  {movieDetail.genres.map((genre) => (
+                    <div key={genre.id} className='movie_tag'>{genre.name}</div>
+                  ))}
+                </div>
+                <div className='my-5'>{movieDetail.overview}</div>
+                {movieDetail.tagline && <div className='text-2xl font-bold text-center text-gray-600'>「{movieDetail.tagline}」</div>}
               </div>
-              {movieDetail.poster_path &&
-                <Image
-                  src={`https://image.tmdb.org/t/p/w500/${movieDetail.poster_path}`}
-                  alt={movieDetail.title}
-                  width={200}
-                  height={200}
-                  className='mx-auto'
+            }
+            {trailerUrl?.results[0]?.key && (
+              <div className="mx-auto">
+                <iframe
+                  src={`https://www.youtube.com/embed/${trailerUrl.results[0]?.key}`}
+                  title="YouTube video player"
+                  frameBorder="0"
+                  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                  allowFullScreen
                 />
-              }
-              <div className='flex'>
-                {movieDetail.genres.map((genre) => (
-                  <div key={genre.id} className='movie_tag'>{genre.name}</div>
-                ))}
               </div>
-              <div className='my-5'>{movieDetail.overview}</div>
-              <div className='text-2xl font-bold text-center text-gray-600'>「{movieDetail.tagline}」</div>
-            </div>
-          }
-          {trailerUrl?.results[0]?.key && (
-            <div>
-              <iframe
-                src={`https://www.youtube.com/embed/${trailerUrl.results[0]?.key}`}
-                title="YouTube video player"
-                frameBorder="0"
-                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-                allowFullScreen
-              />
-            </div>
-          )}
+            )}
+          </div>
         </div>
       </div>
     </Layout>
