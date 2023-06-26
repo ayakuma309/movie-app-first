ad9e23386dbc76a9cb50e6543a2842ab8c8d491d
ayakuma309 <ayako.30901@gmail.com>
2023-06-26 10:35:31 +0900

    fix: ボタンをclickしてデータを取得してる間にloadingを表示した

--- src/components/search/MovieItem.tsx
+++ src/components/search/MovieItem.tsx
@@ -1,13 +1,16 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { MovieItemProps, RecommendProps } from '../../types/Types';
 import { NextPage } from 'next';
 import { useRouter } from 'next/router';
 import Image from 'next/image';
+import { Circles } from 'react-loader-spinner';
 
 const RecommendItem: NextPage<MovieItemProps> = ({ movies }) => {
+  const [isLoading, setIsLoading] = useState(false)
   const router = useRouter();
 
   const onClickSelect = (movie: RecommendProps) => {
+    setIsLoading(true);
     router.push(`/movie/${movie.id}`);
   }
 
@@ -24,14 +27,28 @@ const RecommendItem: NextPage<MovieItemProps> = ({ movies }) => {
               height={300}
             />
             <p className="text-center font-bold text-gray-900 text-lg my-2">{movie.title}</p>
-            <button
-              className="flex items-center justify-center w-full my-2 px-10 py-4 text-base font-medium text-center text-neutral-900 transition duration-500 ease-in-out transform bg-gradient-to-r from-indigo-600 to-indigo-300 rounded-xl hover:from-indigo-300 hover:to-indigo-600 hover:text-white"
-              onClick={() => {
-                onClickSelect(movie);
-              }}
-              >
-              おすすめを探す！
-            </button>
+            {isLoading ? (
+              <>
+                <Circles
+                  height="80"
+                  width="80"
+                  color="#A55EAD"
+                  ariaLabel="circles-loading"
+                  wrapperStyle={{}}
+                  wrapperClass=""
+                  visible={true}
+                />
+              </>
+            ) : (
+              <button
+                className="flex items-center justify-center w-full my-2 px-10 py-4 text-base font-medium text-center text-neutral-900 transition duration-500 ease-in-out transform bg-gradient-to-r from-indigo-600 to-indigo-300 rounded-xl hover:from-indigo-300 hover:to-indigo-600 hover:text-white"
+                onClick={() => {
+                  onClickSelect(movie);
+                }}
+                >
+                おすすめを探す！
+              </button>
+            )}
           </div>
         ))}
     </div>
